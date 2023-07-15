4200297af356e1ecdb3915660d822f875fdbf379
Rustem Kakimov <ruskakimov@gmail.com>
2023-07-15 13:31:31 +0600

    Style post previews

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,9 +8,7 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Meta />
-      <main className="min-h-screen max-w-2xl mx-auto bg-red-300 p-4">
-        {children}
-      </main>
+      <main className="min-h-screen max-w-2xl mx-auto p-4">{children}</main>
     </>
   );
 };
--- components/post-list.tsx
+++ components/post-list.tsx
@@ -9,10 +9,7 @@ type Props = {
 const PostList = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
-      <div className="flex flex-col gap-y-4">
+      <div className="flex flex-col gap-y-16">
         {posts.map((post) => (
           <PostPreview key={post.slug} {...post} />
         ))}
@@ -24,19 +21,15 @@ const PostList = ({ posts }: Props) => {
 const PostPreview = ({ title, date, excerpt, slug }: Post) => {
   return (
     <div>
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
+      <div className="text-sm text-gray-400">
+        <DateFormatter dateString={date} />
+      </div>
+      <h3 className="text-xl font-bold my-2 leading-snug">
+        <Link as={`/posts/${slug}`} href="/posts/[slug]" className="underline">
           {title}
         </Link>
       </h3>
-      <div className="text-lg mb-4">
-        <DateFormatter dateString={date} />
-      </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p className="text-lg text-gray-800">{excerpt}</p>
     </div>
   );
 };
