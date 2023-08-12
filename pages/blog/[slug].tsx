5e8445bb781d925fac8b675759efa53c3510335d
Rustem Kakimov <ruskakimov@gmail.com>
2023-08-12 13:04:12 +0600

    Bigger font on mobile

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -24,7 +24,7 @@ export default function PostPage({ post }: Props) {
   return (
     <article
       className={classNames(
-        "prose sm:prose-xl prose-stone",
+        "prose prose-lg sm:prose-xl prose-stone",
         "prose-h1:tracking-tight",
         "prose-p:leading-relaxed",
         "prose-a:decoration-stone-300 hover:prose-a:decoration-red-500",
