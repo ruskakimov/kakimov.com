e85bb3262c93be68efe4e6b5a73c92e2b4afe901
Rustem Kakimov <ruskakimov@gmail.com>
2023-08-12 13:12:33 +0600

    Link back to homepage

--- components/shell/header.tsx
+++ components/shell/header.tsx
@@ -1,19 +1,25 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="px-4 py-16">
-      <div className="flex flex-row items-center gap-4">
-        <img
-          src="/assets/profile_pic.jpg"
-          className="w-16 aspect-square rounded-full"
-          alt="Rustem Kakimov profile picture"
-        />
-        <div className="flex flex-col gap-0">
-          <span className="text-lg font-semibold">Blog by Rustem Kakimov.</span>
-          <span className="leading-snug">
-            A software developer building interactive experiences.
-          </span>
+      <Link href="/">
+        <div className="flex flex-row items-center gap-4">
+          <img
+            src="/assets/profile_pic.jpg"
+            className="w-16 aspect-square rounded-full"
+            alt="Rustem Kakimov profile picture"
+          />
+          <div className="flex flex-col gap-0">
+            <span className="text-lg font-semibold">
+              Blog by Rustem Kakimov.
+            </span>
+            <span className="leading-snug">
+              A software developer building interactive experiences.
+            </span>
+          </div>
         </div>
-      </div>
+      </Link>
     </header>
   );
 };
