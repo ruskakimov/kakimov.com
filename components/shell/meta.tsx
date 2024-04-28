307a8e91f19f79386f8d2998ab481eda64e902bc
Rustem Kakimov <ruskakimov@gmail.com>
2024-04-28 19:56:14 -0400

    Remove profile picture from og:image

--- components/shell/meta.tsx
+++ components/shell/meta.tsx
@@ -33,7 +33,6 @@ const Meta = () => {
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta name="description" content={HOME_TITLE} />
-      <meta property="og:image" content="/assets/profile_pic.jpg" />
     </Head>
   );
 };
